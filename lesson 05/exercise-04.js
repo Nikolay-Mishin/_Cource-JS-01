ad25757ecc377c386cfda46c73278ac6b18a18b1
Николай Мишин <NIKMIS@live.spbstu.ru>
2019-05-23 20:18:40 +0300

    1

--- lesson 05/exercise-04.js
+++ lesson 05/exercise-04.js
@@ -1,9 +1,3 @@
-function msg (n, text) {
-    console.log (`===== | Задание № ${n} | =====`);
-    console.log (text);
-    console.log ('');
-}
-
 // https://repl.it/@chl9252/DZ-5-4
 
 /* №4 (***)
@@ -13,8 +7,40 @@ function msg (n, text) {
   - Задание творческое. Суть заключается в том чтобы пояснить как функционирует базовый `Array`. По этому не нужно реализовывать все методы что есть в `Array`. 3-5 методов достаточно. Лучше выберите самые сложные для себя.
   - Напишите небольшой скрипт с использование своего класса. Покажите что методы экземпляра класса `Massiv` работают так же как и методы экземпляров класса `Array`. */
 
-function exercise_4 () {
-    //
+class Massiv {
+    constructor () {
+        this.length = arguments.length
+        for (let i=0; i<arguments.length; i++) {
+            this[i] = arguments[i]
+        }
+    }
+
+    push (element) {
+        this[this.length] = element
+        this.length += 1
+    }
+    
+    join (separator) {
+        let str = ''
+        for (let i=0; i< this.length-1; i++) {
+            str += this[i] + separator
+        }
+        return str + this[this.length-1]
+    }
+
+    find (callback) {
+        for (let i=0; i< this.length; i++) {
+            const result = callback(this[i])
+            if (result) {
+                return this[i]
+            }
+        }
+    }
 }
 
-msg (4, exercise_4());
+const massiv = new Massiv(1,2,3,4,5)
+massiv.push(7)
+
+console.log(massiv)
+console.log(massiv.join(', '))
+console.log(massiv.find(e => e % 2 === 0))
